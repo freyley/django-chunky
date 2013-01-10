de7bb1035bc3a62f47b51b08f7b1921e8f999c1a
Jeff Schwaber <jeff@lofiart.com>
2013-01-10 12:17:51 -0800

    remove old code from setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,5 @@
 from setuptools import setup
 
-import os
-
-def get_source_files():
-    source_files = []
-    for dirname, _, files in os.walk('chunky'):
-        for filename in files:
-            if not filename.endswith('.pyc'):
-                source_files.append( os.path.join('/'.join(dirname.split('/')[1:]), filename))
-    return source_files
-
 setup(
     name='django-chunky',
     version='0.1.7',
